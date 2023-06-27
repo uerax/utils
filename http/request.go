11ffba9519037f72ea803146c244cda0bb44adce
wesker <uerax@live.com>
2023-06-28 02:36:39 +0800

    add postt

--- http/request.go
+++ http/request.go
@@ -6,15 +6,60 @@ import (
 	"net/http"
 )
 
-func Get(url string, t any) error {
-	r, err := http.Get(url)
+func Get(url string, parameter map[string]string, header map[string]string, t any) error {
+	
+	if len(parameter) != 0 {
+		if url[len(url) - 1] != '?' {
+			url += "?"
+		}
+		for k, v := range parameter {
+			if url[len(url) - 1] != '?' {
+				url += "&"
+			}
+			url += k + "=" + v
+		}
+	}
+
+	r, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return err
 	}
+
+	for k, v := range header {
+		r.Header.Set(k, v)
+	}
+
 	b, err := io.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
 		return err
 	}
 	return json.Unmarshal(b, t)
+}
+
+
+
+func Post(url string, parameter io.Reader, header map[string]string, t any) error {
+	req, err := http.NewRequest(http.MethodPost, url, parameter)
+	if err != nil {
+		return err
+	}
+
+	for k, v := range header {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(b, &t)
+
 }
